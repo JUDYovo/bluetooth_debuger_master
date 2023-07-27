6e4dd46e9777cc9d920f34a46877efd043ef6abc
JUDYovo <12110946@mail.sustech.edu.cn>
2023-07-27 15:00:26 +0800

    first commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'bluetooth_rx_test2' 
+ * Target:  'bluetooth_rx_test2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/bluetooth_rx_test2/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+bluetooth_rx_test2\startup_stm32f103xe.o: startup_stm32f103xe.s
